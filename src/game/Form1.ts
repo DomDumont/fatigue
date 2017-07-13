74e83bf35cafebb4de5d1051dea1cab49517e57f
DomDom <dom.dumont@gmail.com>
2017-07-13 12:07:30 +0200

    pouet

--- src/game/Form1.ts
+++ src/game/Form1.ts
@@ -2,4 +2,12 @@ import Form from "../engine/PIXIForms/Form";
 
 export default class Form1 extends Form {
 
+    public constructor() {
+        super();
+        this.InitializeComponent();
+    }
+
+    private InitializeComponent(): void {
+        // todo
+    }
 }
