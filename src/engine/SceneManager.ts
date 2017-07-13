625bdc5c8a49b77096dd399b30dcb7f81f2b4555
DomDom <dom.dumont@gmail.com>
2017-07-13 11:26:12 +0200

    lint

--- src/engine/SceneManager.ts
+++ src/engine/SceneManager.ts
@@ -1,106 +1,100 @@
-import * as PIXI from 'pixi.js';
-import {Scene} from "./Scene";
-import * as PimGUI from './PimGUI';
-import 'stats.js';
+import * as PIXI from "pixi.js";
+import { Scene } from "./Scene";
+import * as PimGUI from "./PimGUI";
+import "stats.js";
 
 
-export class SceneManager 
-{
+export class SceneManager {
 
     private static instance: SceneManager;
     private stats = new Stats();
-    public gui:PimGUI.PimGUI;
+    public gui: PimGUI.PimGUI;
 
-    static Get() 
-        {
-        if (!SceneManager.instance) 
-            {
+    static Get():SceneManager {
+        if (!SceneManager.instance) {
             SceneManager.instance = new SceneManager();
             // ... any one time initialization goes here ...
-            }
-        return SceneManager.instance;
         }
+        return SceneManager.instance;
+    }
 
-    private constructor()
-    {
+    private constructor() {
         this.gui = new PimGUI.PimGUI();
     }
 
-    private  scenes: any = {}; // should be hashmap but a JS object is fine too :)
-    public  currentScene: Scene;
-    public  renderer: PIXI.WebGLRenderer|PIXI.CanvasRenderer;
+    private scenes: any = {}; // should be hashmap but a JS object is fine too :)
+    public currentScene: Scene;
+    public renderer: PIXI.WebGLRenderer | PIXI.CanvasRenderer;
 
-    
 
-    public Create(width: number, height: number, color:number)
-    {
-        if (this.renderer) 
-            {
+
+    public Create(width: number, height: number, color: number):SceneManager {
+        if (this.renderer) {
             return this;
-            }
+        }
 
         this.renderer = PIXI.autoDetectRenderer(width, height);
         (this.renderer as any).backgroundColor = color;
-        //document.body.appendChild(this.renderer.view);
+        // document.body.appendChild(this.renderer.view);
         this.DisableContextMenu(this.renderer.view);
-        document.getElementById('gameContainer').appendChild(this.renderer.view);
-    
+        document.getElementById("gameContainer").appendChild(this.renderer.view);
+
 
 
         requestAnimationFrame(this.Loop);
 
-        this.stats.showPanel( 0 ); // 0: fps, 1: ms, 2: mb, 3+: custom
-        this.stats.dom.style.position = 'absolute';
-		this.stats.dom.style.top = '10px';
-        this.stats.dom.style.left = '10px';
-        //document.body.appendChild( this.stats.dom );     
-        document.getElementById('fpsCounter').appendChild( this.stats.dom );      
+        this.stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
+        this.stats.dom.style.position = "absolute";
+        this.stats.dom.style.top = "10px";
+        this.stats.dom.style.left = "10px";
+        // document.body.appendChild( this.stats.dom );
+        document.getElementById("fpsCounter").appendChild(this.stats.dom);
         return this;
     }
 
-    public DisableContextMenu(canvas) 
-        {
-        canvas.addEventListener('contextmenu', (e) => {
-        e.preventDefault();
+    public DisableContextMenu(canvas:any):void {
+        canvas.addEventListener("contextmenu", (e) => {
+            e.preventDefault();
         });
-        }
-    private  Loop = () => {
+    }
+    private Loop = () => {
         this.stats.begin();
         requestAnimationFrame(this.Loop);
 
 
-        if (!this.currentScene || this.currentScene.isPaused()) return;
+        if (!this.currentScene || this.currentScene.isPaused()) {
+            return;
+        }
 
 
         this.currentScene.Update();
         this.renderer.render(this.gui);
-        
-    
+
+
         this.stats.end();
     }
 
     public CreateScene<A extends Scene>(id: string, construct: new () => A): A {
-        if (this.scenes[id]) return undefined;
+        if (this.scenes[id]) {
+            return undefined;
+        }
 
-        var scene = new construct();
+        var scene: A = new construct();
         scene.gameManager = this;
         this.scenes[id] = scene;
 
         return scene;
     }
- 
-    public GoToScene(id: string): boolean
-    {
 
-        if (this.scenes[id]) 
-        {
-            if (this.currentScene) 
-                {
-                //Pause old scene and remove it from the GUI
+    public GoToScene(id: string): boolean {
+
+        if (this.scenes[id]) {
+            if (this.currentScene) {
+                // pause old scene and remove it from the GUI
                 this.gui.removeChild(this.currentScene);
                 this.currentScene.Pause();
             }
-            //Set currentScene, add it to the GUI node, and resume it
+            // set currentScene, add it to the GUI node, and resume it
             this.currentScene = this.scenes[id];
             this.gui.addChild(this.currentScene);
             this.currentScene.Resume();
--- src/engine/TileSet.ts
+++ src/engine/TileSet.ts
@@ -1,18 +1,16 @@
 
 
-export class TileSet
-{
-    public nbTilesX:number;
-    public nbTilesY:number;
-    public texture:PIXI.BaseTexture;
-    public sizeX:number;
-    public sizeY:number;
-    //public sprites:PIXI.Sprite[][];
-    
+export class TileSet {
+    public nbTilesX: number;
+    public nbTilesY: number;
+    public texture: PIXI.BaseTexture;
+    public sizeX: number;
+    public sizeY: number;
+    // public sprites:PIXI.Sprite[][];
 
-    constructor(_texture:PIXI.BaseTexture, _sizeX:number, _sizeY:number)
-    {
-        
+
+    constructor(_texture: PIXI.BaseTexture, _sizeX: number, _sizeY: number) {
+
         this.texture = _texture;
         this.sizeX = _sizeX;
         this.sizeY = _sizeY;
@@ -20,17 +18,16 @@ export class TileSet
         this.nbTilesX = this.texture.width / this.sizeX;
         this.nbTilesY = this.texture.height / this.sizeY;
 
-        
+
     }
 
-    public CreateSprite(spriteIndex:number):PIXI.Sprite
-    {
-        let x = spriteIndex % this.nbTilesX;        
-        let y = Math.floor(spriteIndex / this.nbTilesX);
-        
+    public CreateSprite(spriteIndex: number): PIXI.Sprite {
+        let x:number = spriteIndex % this.nbTilesX;
+        let y:number = Math.floor(spriteIndex / this.nbTilesX);
 
-    //this.sprites[i][j] = new PIXI.Sprite(texture);
-    return new PIXI.Sprite(new PIXI.Texture(this.texture,new PIXI.Rectangle(x * this.sizeX,y * this.sizeY, this.sizeX, this.sizeY)));
-        
+
+        // this.sprites[i][j] = new PIXI.Sprite(texture);
+        return new PIXI.Sprite(new PIXI.Texture(this.texture, new PIXI.Rectangle(x * this.sizeX, y * this.sizeY, this.sizeX, this.sizeY)));
+
     }
 }
