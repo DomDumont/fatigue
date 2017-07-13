946965f6dbe8046ead6af2594b3ede16344feeda
DomDom <dom.dumont@gmail.com>
2017-07-13 19:47:54 +0200

    prouto

--- src/engine/PIXIForms/Utils.ts
+++ src/engine/PIXIForms/Utils.ts
@@ -10,15 +10,35 @@ export class Size {
     }
 }
 
+export class EventArg{
+
+}
 interface EventHandler {
-    ( error: Error, result?: number ) : void;
+    ( sender: object , arg: EventArg ) : void;
 }
 
 export class Event
 {
+    private handlers : EventHandler [];
+
+    constructor() {
+		this.handlers = [];
+	}
+
     public Add( newHandler:EventHandler)
     {
-
+    this.handlers.push(newHandler);
     }
+    public Remove (handler : EventHandler) : void {
+		this.handlers.splice(this.handlers.indexOf(handler), 1);
+	}
+
+    public Raise (sender: object , arg: EventArg) : void 
+    {
+
+		this.handlers.forEach((handler : EventHandler)=> {
+			handler(sender,arg);
+		});
+	}
 
 }
