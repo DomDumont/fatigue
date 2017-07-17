abc85da5eda819b8d8ed7cd44d64d6e1f981ef4e
DomDom <dom.dumont@gmail.com>
2017-07-17 16:28:07 +0200

    plop

--- src/engine/PIXIForms/Application.ts
+++ src/engine/PIXIForms/Application.ts
@@ -4,26 +4,27 @@ export default class Application {
     private static _ready:any;
     public static renderer: PIXI.WebGLRenderer | PIXI.CanvasRenderer;
     private static stats = new Stats();
+    public static CurrentForm:Form;
 
-    public static Initialize(width:number,height:number,color:number)
+    public static Initialize(width:number,height:number,rototo:number)
     {
     Application.renderer = PIXI.autoDetectRenderer(width, height);
-    (Application.renderer as any).backgroundColor = color;
-    Application.DisableContextMenu(Application.renderer.view);
+    (Application.renderer as any).backgroundColor = rototo;
+    //Application.DisableContextMenu(Application.renderer.view);
     document.getElementById("PIXIForms").appendChild(Application.renderer.view);
     requestAnimationFrame(Application.Loop);
 
     Application.stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
     Application.stats.dom.style.position = "absolute";
     Application.stats.dom.style.top = "10px";
-    Application.stats.dom.style.left = "10px";
-    // document.body.appendChild( this.stats.dom );
+    Application.stats.dom.style.left = "700px";
+    document.body.appendChild( this.stats.dom );
     document.getElementById("PIXIFormsFPS").appendChild(Application.stats.dom);
 
     PIXI.loader.add("../data/fonts/Proggy.xml").load(Application.OnLoadFinished);
     }
     public static Run(mainForm: Form): void {
- 
+        Application.CurrentForm = mainForm;
     }    
 
     public static Ready(callback:any)
@@ -55,10 +56,10 @@ export default class Application {
         this.currentScene.Update();
         this.renderer.render(this.gui);
         */
-
+        Application.renderer.render(Application.CurrentForm);
         Application.stats.end();
     }
 
 }
 
-Application.Initialize(800,600,0xff0000);
+Application.Initialize(800,600,0x000000);
